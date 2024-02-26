e2df72c102099e1d1ddf2ef661c3c9e2166e0121
Aleksey <159144620+afilyuta@users.noreply.github.com>
2024-02-26 13:58:08 +0300

    added game selection logic

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,26 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 import static hexlet.code.Cli.greetingUser;
 
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        greetingUser();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(
+                "Please enter the game number and press Enter.\n"
+                + "1 - Greet\n"
+                + "0 - Exit"
+        );
+
+        int gameNumber = scanner.nextInt();
+        System.out.println("Your choice: " + gameNumber + "\n");
+
+        if (gameNumber == 1) {
+            System.out.println("Welcome to the Brain Games!");
+            greetingUser();
+        }
     }
 }
