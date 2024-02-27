b3294c31c424ff326ac0a32e5fd9db606c95bfa0
Aleksey <159144620+afilyuta@users.noreply.github.com>
2024-02-27 23:50:10 +0300

    Refactoring code

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Cli {
 
-    public static Cli cli = new Cli();
-    public String userName;
+    public static String userName;
 
     public static void greetingUser() {
-        System.out.println("\nWelcome to the Brain Games!");
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("\nWelcome to the Brain Games!");
         System.out.print("May I have your name? ");
 
-        cli.userName = scanner.next();
-        System.out.println("Hello, " + cli.userName + "!");
+        userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
     }
+
 }
