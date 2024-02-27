767de0da1402586ce37a75373480a5b943af8154
Aleksey <159144620+afilyuta@users.noreply.github.com>
2024-02-27 23:52:44 +0300

    Create game's architecture

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,4 +1,36 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
+import static hexlet.code.Cli.greetingUser;
+
 public class Engine {
+
+    public static void runGame(String[][] expressionsAndRightAnswers) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (String[] part : expressionsAndRightAnswers) {
+            String expression = part[0];
+            String rightAnswer = part[1];
+
+            System.out.println("Question: " + expression);
+            System.out.print("Your answer: ");
+            String answer = scanner.next();
+
+            if (answer.equalsIgnoreCase(rightAnswer)) {
+                System.out.println("Correct!");
+            } else {
+                System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'.", answer, rightAnswer);
+                System.out.printf("Let's try again, %s\n", Cli.userName);
+                System.exit(0);
+            }
+        }
+
+        System.out.printf("Congratulations, %s!\n", Cli.userName);
+    }
+
+    public static void getGreetingAndRules(String rules) {
+        greetingUser();
+        System.out.println(rules);
+    }
 }
