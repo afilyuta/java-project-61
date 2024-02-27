f6c807470293cb819c00c880886c0f39dd55521b
Aleksey <159144620+afilyuta@users.noreply.github.com>
2024-02-27 13:08:22 +0300

    make refactoring of class App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 import static hexlet.code.Cli.greetingUser;
 
-import static hexlet.code.Even.even;
+import static hexlet.code.games.Even.even;
 
 public class App {
 
@@ -15,16 +15,20 @@ public class App {
                 "Please enter the game number and press Enter.\n"
                 + "1 - Greet\n"
                 + "2 - Even\n"
+                + "3 - Calc\n"
                 + "0 - Exit"
         );
 
         System.out.print("Your choice: ");
         int gameNumber = scanner.nextInt();
 
-        if (gameNumber == 1) {
-            greetingUser();
-        } else if (gameNumber == 2) {
-            even();
+        switch (gameNumber) {
+            case 1:
+                greetingUser();
+            case 2:
+                even();
+            case 3:
+
         }
     }
 }
