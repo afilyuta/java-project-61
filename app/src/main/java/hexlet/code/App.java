d2d3ad7cc2b5181bf8fda304c084d1737677f969
Aleksey <159144620+afilyuta@users.noreply.github.com>
2024-02-28 02:29:54 +0300

    Update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,6 +6,8 @@ import static hexlet.code.Cli.greetingUser;
 
 import static hexlet.code.games.Even.even;
 
+import static hexlet.code.games.Calc.calc;
+
 
 public class App {
 
@@ -16,16 +18,25 @@ public class App {
                 "Please enter the game number and press Enter.\n"
                 + "1 - Greet\n"
                 + "2 - Even\n"
+                + "3 - Calc\n"
                 + "0 - Exit"
         );
 
         System.out.print("Your choice: ");
         int gameNumber = scanner.nextInt();
 
-        if (gameNumber == 1) {
-            greetingUser();
-        } else if (gameNumber == 2) {
-            even();
+        switch (gameNumber) {
+            case 1:
+                greetingUser();
+                break;
+            case 2:
+                even();
+                break;
+            case 3:
+                calc();
+                break;
+            default:
+                System.exit(0);
         }
     }
 }
