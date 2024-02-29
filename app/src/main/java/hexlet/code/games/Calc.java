e54bc10262c1612be6dd98a0e93cf2b687d5c036
Aleksey <159144620+afilyuta@users.noreply.github.com>
2024-03-01 02:22:50 +0300

    Update Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -9,38 +9,41 @@ public class Calc {
 
     public static void calc() {
         getGreetingAndRules("What is the result of the expression?");
-        String[][] expressionsAndRightAnswers = getExpressionsAndRightAnswers();
-        runGame(expressionsAndRightAnswers);
+        String[][] gamePack = getGamePack();
+        runGame(gamePack);
     }
 
-    public static String[][] getExpressionsAndRightAnswers() {
-        String[][] expressionsAndRightAnswers = new String[3][2];
-        int length = expressionsAndRightAnswers.length;
+    public static String[] getRoundPack() {
+        int a = getTwoDigitNumber();
+        int b = getSingleDigitNumber();
+        int c = getSingleDigitNumber();
+
+        String sign = switch (c) {
+            case 1, 4, 7 -> "+";
+            case 2, 5, 8 -> "-";
+            default -> "*";
+        };
+
+        int intRightAnswer = switch (c) {
+            case 1, 4, 7 -> a + b; // +
+            case 2, 5, 8 -> a - b; // -
+            default -> a * b;
+        };
+
+        String expression = a + " " + sign + " " + b;
+        String rightAnswer = "" + intRightAnswer;
+
+        return new String[]{expression, rightAnswer};
+    }
+
+    public static String[][] getGamePack() {
+        String[][] gamePack = new String[3][2];
+        int length = gamePack.length;
 
         for (int i = 0; i < length; i++) {
-            int a = getTwoDigitNumber();
-            int b = getSingleDigitNumber();
-            int c = getSingleDigitNumber();
-
-            String sign = switch (c) {
-                case 1, 4, 7 -> "+";
-                case 2, 5, 8 -> "-";
-                default -> "*";
-            };
-
-            int intRightAnswer = switch (c) {
-                case 1, 4, 7 -> a + b; // +
-                case 2, 5, 8 -> a - b; // -
-                default -> a * b;
-            };
-
-            String expression = a + " " + sign + " " + b;
-            String rightAnswer = "" + intRightAnswer;
-
-            String[] expressionAndRightAnswer = {expression, rightAnswer};
-            expressionsAndRightAnswers[i] = expressionAndRightAnswer;
+            gamePack[i] = getRoundPack();
         }
 
-        return expressionsAndRightAnswers;
+        return gamePack;
     }
 }
