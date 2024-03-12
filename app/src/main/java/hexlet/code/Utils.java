4eedaa393a559fbc905572268a54552d03622bb6
Aleksey <159144620+afilyuta@users.noreply.github.com>
2024-03-12 21:52:54 +0300

    Update Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Random;
 
 public class Utils {
-    private static Random random = new Random();
+    private static final Random random = new Random();
 
     public static int generateRandomInt(int bound) {
         return random.nextInt(bound);
@@ -11,9 +11,5 @@ public class Utils {
 
     public static int generateRandomInt(int min, int max) {
         return random.nextInt(max - min + 1) + min;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(generateRandomInt(5, 15));
     }
 }
