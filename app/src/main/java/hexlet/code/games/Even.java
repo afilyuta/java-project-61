0548ced278794adf38eaabd7c3009b592f4b240f
Aleksey <159144620+afilyuta@users.noreply.github.com>
2024-03-12 22:47:36 +0300

    Update Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -6,6 +6,7 @@ import static hexlet.code.Utils.generateRandomInt;
 public class Even {
 
     private static final int COUNT_OF_QUESTIONS = 3;
+    private static final int TWO_DIGIT_NUMBER_GENERATOR_BOUNDARY = 99;
 
     public static void runEvenGame() {
         String[][] questionsAndAnswers = getQuestionsAndAnswers();
@@ -16,7 +17,7 @@ public class Even {
         String[][] questionsAndAnswers = new String[2][COUNT_OF_QUESTIONS];
 
         for (int i = 0; i < COUNT_OF_QUESTIONS; i++) {
-            int a = generateRandomInt(1,99);
+            int a = generateRandomInt(1, TWO_DIGIT_NUMBER_GENERATOR_BOUNDARY);
 
             String question = "" + a;
             String answer = Integer.parseInt(question) % 2 == 0 ? "yes" : "no";
