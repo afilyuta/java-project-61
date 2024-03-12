7dd76f40557172910d5408fbcbd61cfa09551784
Aleksey <159144620+afilyuta@users.noreply.github.com>
2024-03-12 21:54:57 +0300

    Update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,56 +3,47 @@ package hexlet.code;
 import java.util.Scanner;
 
 import static hexlet.code.Cli.greetingUser;
-import static hexlet.code.games.Even.even;
-import static hexlet.code.games.Calc.calc;
-import static hexlet.code.games.GCD.gcd;
-import static hexlet.code.games.Progression.progression;
-import static hexlet.code.games.Prime.prime;
+import static hexlet.code.games.Even.runEvenGame;
+import static hexlet.code.games.Calc.runCalcGame;
+import static hexlet.code.games.GCD.runGcdGame;
+import static hexlet.code.games.Progression.runProgressionGame;
+import static hexlet.code.games.Prime.runPrimeGame;
 
 public class App {
+
+    public static final int GREET_GAME = 1;
+    public static final int EVEN_GAME = 2;
+    public static final int CALC_GAME = 3;
+    public static final int GSD_GAME = 4;
+    public static final int PROGRESSION_GAME = 5;
+    public static final int PRIME_GAME = 6;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println(
-                "Please enter the game number and press Enter.\n"
-                + "1 - Greet\n"
-                + "2 - Even\n"
-                + "3 - Calc\n"
-                + "4 - GCD\n"
-                + "5 - Progression\n"
-                + "6 - Prime\n"
-                + "0 - Exit"
+                """
+                        Please enter the game number and press Enter.
+                        1 - Greet
+                        2 - Even
+                        3 - Calc
+                        4 - GCD
+                        5 - Progression
+                        6 - Prime
+                        0 - Exit"""
         );
 
         System.out.print("Your choice: ");
-        chooseGame(scanner);
-    }
+        int numberEnteredByPlayer = scanner.nextInt();
 
-    private static void chooseGame(Scanner scanner) {
-        int playerChoice = scanner.nextInt();
-
-        int greet = 1;
-        int even = 2;
-        int calc = 3;
-        int gcd = 4;
-        int progression = 5;
-        int prime = 6;
-
-        if (playerChoice == greet) {
-            greetingUser();
-        } else if (playerChoice == even) {
-            even();
-        } else if (playerChoice == calc) {
-            calc();
-        } else if (playerChoice == gcd) {
-            gcd();
-        } else if (playerChoice == progression) {
-            progression();
-        } else if (playerChoice == prime) {
-            prime();
+        switch (numberEnteredByPlayer) {
+            case GREET_GAME -> greetingUser();
+            case EVEN_GAME -> runEvenGame();
+            case CALC_GAME -> runCalcGame();
+            case GSD_GAME -> runGcdGame();
+            case PROGRESSION_GAME -> runProgressionGame();
+            case PRIME_GAME -> runPrimeGame();
+            default -> System.exit(0);
         }
-
-        System.exit(0);
     }
 }
